0b61280eb30f2a8f53e3edf5b888f4efc3d2afb6
november-golf <n.s.gray@live.co.uk>
2022-11-08 14:54:45 +0000

    Task 4 complete.

--- task1.js
+++ task1.js
@@ -1,3 +1,20 @@
-const cats = ["tony", "daisy", "socks", "rockie" ];
+const cats = ["tony", "daisy", "socks", "rockie"];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map( (eachCat) => {
+    
+    //return eachCat.charAt(0).toUpperCase();
+   
+    return eachCat[0].toUpperCase
+     })
+
+// your code here
+
+// Iterate over the array
+    // Use map to iterate over each item.
+    // cats.map( (eachCat) => {
+       //eachCat[0].toUpperCase(); 
+        //})
+
+// Capitalise the first letter of each word
+// Return the new updated array.
+
